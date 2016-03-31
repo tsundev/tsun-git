46dbfa4b9b7553e3784ceea67004afdd10cc5772
Ryuko <tombarber@Toms-Mac-mini.local>
2016-03-31 09:40:42 +0100

    TsunGit
    Here’s the first commit.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en-GB">
+
+<head>
+	<title>Test</title>
+	<link rel="shortcut icon" href="#">
+	<link rel="stylesheet" href="#">
+	<script src="#"></script>
+	<meta name="description" content="Nope">
+	<meta name="keywords" content="Test">
+	<meta name="author" content="TsunDev">
+	<meta charset="UTF-8">
+</head>
+
+<body>
+<div class="row">
+	<h1>TsunGit</h1>
+</div>
+</body>
+</html>
